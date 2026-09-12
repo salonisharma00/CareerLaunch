bd4247ae326e87038585c22a1c245cd846573187
salonisharma00 <vini95878@gmail.com>
2026-09-12 20:58:37 +0530

    Enable production debug settings

--- placement_project/settings.py
+++ placement_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t46wt-%tq(jg-95i_&-jzd8%(zmd2&grpkb1v+ud4bw4%xjqjp'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
